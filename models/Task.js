07e2fcf3d301b5123f96c0e4dc6f7a515ecb221a
waltodd <waltodd@hotmail.com>
2024-10-31 10:22:07 +0100

    add description field into model Task

--- models/Task.js
+++ models/Task.js
@@ -3,6 +3,7 @@ import mongoose  from "mongoose";
 const TaskSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   title: { type: String, required: true },
+  description: { type: String, required: true },
   priority: { type: String, enum: ["baixa", "media", "alta"], default: "medium" },
   completed: { type: Boolean, default: false },
 });
